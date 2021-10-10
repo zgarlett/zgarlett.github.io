8be59ba0b56ea7ec8aa5e4504c1ec1528ddf39dd
zgarlett <zachary.garlett@okstate.edu>
2021-10-09 21:24:35 -0500

    staged

---
+++ src/components/demo.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export function Demo() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <div className="banner">
+                <p>
+                    My name is <code>Zachary Garlett</code> and I am a </p><p><code>Software Engineer.</code>
+                </p>
+                </div>
+            </header>
+        </div>
+    );
+}
+
---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export * from './demo';
+export * from './header';
+export * from './resume';
+export * from './work';
